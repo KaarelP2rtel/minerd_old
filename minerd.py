6df56b0b0ee1392c943682b2fc3f965c20d88f7c
Kaarel Pärtel <kaarelp2rtel@gmail.com>
2017-05-24 14:42:15 +0300

    Uncommented serial again. Code now PEP8 complaint with the exception of Line too long

--- minerd.py
+++ minerd.py
@@ -5,68 +5,69 @@ from functools import wraps
 import requests
 from flask import Flask, render_template, request, Response
 
+
 class Apis:
-    uptime="Loading"
-    hashrate="Loading"
-    balance="Loading"
-    eurprice="Loading"
-    usdprice="Loading"
-    zecprice="Loading"
-    profit="Loading"
-    miner1="Ylo"
-    balEur=""
-    profEur=""
-    status=""
-   
+    uptime = "Loading"
+    hashrate = "Loading"
+    balance = "Loading"
+    eurprice = "Loading"
+    usdprice = "Loading"
+    zecprice = "Loading"
+    profit = "Loading"
+    miner1 = "Ylo"
+    balEur = ""
+    profEur = ""
+    status = ""
+
     def apiGet(self):
         def upString(minutes):
-            minutes=int(minutes)
-            r=""
-            dys=int(minutes/1440)
-            hrs=int((minutes-dys*1440)/60)
-            mns=minutes-dys*1440-hrs*60
-            if dys==1:
-                days=" day "
+            minutes = int(minutes)
+            r = ""
+            dys = int(minutes/1440)
+            hrs = int((minutes-dys*1440)/60)
+            mns = minutes-dys*1440-hrs*60
+            if dys == 1:
+                days = " day "
             else:
-                days=" days "
-            if hrs==1:
-                hours=" hour "
+                days = " days "
+            if hrs == 1:
+                hours = " hour "
             else:
-                hours=" hours "
-            if mns==1:
-                mins=" minute"
+                hours = " hours "
+            if mns == 1:
+                mins = " minute"
             else:
-                mins=" minutes"
+                mins = " minutes"
             if dys:
-                r+=str(dys)+days
+                r += str(dys)+days
             if hrs:
-                r+=str(hrs)+hours
+                r += str(hrs)+hours
             if mns:
-                r+=str(mns)+mins
+                r += str(mns)+mins
             return r
 
         try:
-            priceApi=requests.get("http://api.coindesk.com/v1/bpi/currentprice.json")
-            minerApi=requests.get("https://api.nicehash.com/api?method=stats.provider&addr=36VitWXAXFyvdKaui9sPuKDnQwdtBWGieV")
-            workerApi=requests.get("https://api.nicehash.com/api?method=stats.provider.workers&addr=36VitWXAXFyvdKaui9sPuKDnQwdtBWGieV&algo=24")
-            zecApi=requests.get("https://api.coinmarketcap.com/v1/ticker/zcash/")
-            profApi=requests.get("https://api.nicehash.com/api?method=stats.provider.ex&addr=36VitWXAXFyvdKaui9sPuKDnQwdtBWGieV")
-            priceData=priceApi.json()
-            minerData=minerApi.json()
-            workerData=workerApi.json()
-            zecData=zecApi.json()
-            profData=profApi.json()
+            priceApi = requests.get("http://api.coindesk.com/v1/bpi/currentprice.json")
+            minerApi = requests.get("https://api.nicehash.com/api?method=stats.provider&addr=36VitWXAXFyvdKaui9sPuKDnQwdtBWGieV")
+            workerApi = requests.get("https://api.nicehash.com/api?method=stats.provider.workers&addr=36VitWXAXFyvdKaui9sPuKDnQwdtBWGieV&algo=24")
+            zecApi = requests.get("https://api.coinmarketcap.com/v1/ticker/zcash/")
+            profApi = requests.get("https://api.nicehash.com/api?method=stats.provider.ex&addr=36VitWXAXFyvdKaui9sPuKDnQwdtBWGieV")
+            priceData = priceApi.json()
+            minerData = minerApi.json()
+            workerData = workerApi.json()
+            zecData = zecApi.json()
+            profData = profApi.json()
         except:
             print("API error")
             return
-        
+
         try:
             self.uptime = upString(workerData["result"]["workers"][0][2])
             self.hashrate = workerData["result"]["workers"][0][1]["a"]
         except:
             self.uptime = "0 minutes"
             self.hashrate = "0"
-    
+
         if self.hashrate == "0":
             self.status = "Fucked"
         else:
@@ -74,47 +75,49 @@ class Apis:
 
         balRaw = 1000*float(minerData["result"]["stats"][2]["balance"])
         eurRaw = priceData["bpi"]["EUR"]["rate_float"]
-        self.balance=str(int(balRaw))+"."+str(int((balRaw-int(balRaw))*100))+" mBTC"
+        self.balance = str(int(balRaw))+"."+str(int((balRaw-int(balRaw))*100))+" mBTC"
         eur = eurRaw*balRaw/1000
-        self.balEur=str(int(eur))+"."+str(int((eur-int(eur))*100))+" €"
+        self.balEur = str(int(eur))+"."+str(int((eur-int(eur))*100))+" €"
         self.usdprice = str(priceData["bpi"]["USD"]["rate_float"])[:7]
-        self.eurprice=str(eurRaw)[:7]
+        self.eurprice = str(eurRaw)[:7]
         self.zecprice = zecData[0]["price_btc"]
-       
+
         try:
-            profRaw=float(profData["result"]["current"][2]["profitability"])*1000*float(self.hashrate)
-            self.profit=str(int(profRaw))+"."+str(int((profRaw-int(profRaw))*100))+" mBTC"
-            prfEur=profRaw*eurRaw/1000
-            self.profEur=str(int(prfEur))+"."+str(int((prfEur-int(prfEur))*100))+" €"
+            profRaw = float(profData["result"]["current"][2]["profitability"])*1000*float(self.hashrate)
+            self.profit = str(int(profRaw))+"."+str(int((profRaw-int(profRaw))*100))+" mBTC"
+            prfEur = profRaw*eurRaw/1000
+            self.profEur = str(int(prfEur))+"."+str(int((prfEur-int(prfEur))*100))+" €"
         except KeyError:
             pass
-        
+
         print("APIs updated")
-        
-    
-#ser=serial.Serial("/dev/ttyUSB0")
 
-file = open("conf","r")
-confU=file.readline()[:-1]
-confP=file.readline()[:-1]
-confR=file.readline()[:-1]
 
-app=Flask(__name__)
+ser = serial.Serial("/dev/ttyUSB0")
 
-apis=Apis()
+file = open("conf", "r")
+confU = file.readline()[:-1]
+confP = file.readline()[:-1]
+confR = file.readline()[:-1]
+
+app = Flask(__name__)
+
+apis = Apis()
+
 
 def refreshApis():
     while True:
-        apis.apiGet()        
+        apis.apiGet()
         time.sleep(30)
-    
+
+
 def check_auth(username, password):
         return username == confU and password == confP
 
+
 def authenticate():
-    return Response(
-    'FUCK OFF', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    return Response('FUCK OFF', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 def requires_auth(f):
     @wraps(f)
@@ -125,26 +128,24 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
-@app.route("/", methods=["GET","POST"])
+
+@app.route("/", methods=["GET", "POST"])
 @requires_auth
 def hello():
-    pwstat=""
-    if request.method=="POST":
-        password=request.form['password']
-        if password==confR:
+    pwstat = ""
+    if request.method == "POST":
+        password = request.form['password']
+        if password == confR:
             print("RESTARTING")
-            pwstat="Restart Successful"
-            #ser.write(b"a")
+            pwstat = "Restart Successful"
+            ser.write(b"a")
             time.sleep(10)
-            #ser.write(b"A")
+            ser.write(b"A")
         else:
-            pwstat="Incorrect Password"
-            
-    return render_template("template.html",profEur=apis.profEur, status=apis.status, profit=apis.profit, uptime=apis.uptime, hashrate=apis.hashrate, balEur=apis.balEur, balance=apis.balance, eurprice=apis.eurprice, usdprice=apis.usdprice, zecprice=apis.zecprice, miner1=apis.miner1, pwstat=pwstat)
+            pwstat = "Incorrect Password"
 
-if __name__=="__main__":
-    _thread.start_new_thread(app.run,("0.0.0.0",80))
+    return render_template("template.html", profEur=apis.profEur, status=apis.status, profit=apis.profit, uptime=apis.uptime, hashrate=apis.hashrate, balEur=apis.balEur, balance=apis.balance, eurprice=apis.eurprice, usdprice=apis.usdprice, zecprice=apis.zecprice, miner1=apis.miner1, pwstat=pwstat)
+
+if __name__ == "__main__":
+    _thread.start_new_thread(app.run, ("0.0.0.0", 80))
     _thread.start_new_thread(refreshApis())
-  
-    
-	 
